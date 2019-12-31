82a0f61b7e7c38076d03c3e618f570b47b039982
moomoo14 <mouhamedbocar@yahoo.fr>
2019-12-31 20:17:20 +0800

    add: dfs code debug

--- homework_1/graph_implementation.py
+++ homework_1/graph_implementation.py
@@ -1,18 +1,5 @@
 from collections import deque, defaultdict
-
-# l = [
-#     (0, 6),
-#     (0, 4),
-#     (0, 2),
-#     (0, 1),
-#     (2, 0),
-#     (3, 5),
-#     (3, 4),
-#     (3, 6),
-#     (4, 5),
-#     (4, 6),
-#     (4, 3)
-# ]
+# import networkx as nx
 
 l = [
     (2, 1),
@@ -25,8 +12,55 @@ l = [
     (8, 9),
     (4, 5),
 ]
+# We create some sample search problems to check our algorithms
+#this class will create a 2D grid of row x colums and its graph.
+#Some of the cells can be disabled by putting it into walls
+#for example SearchGrid(3,4,walls=[0,10]) will create a grid that has cell 0 and 10 are disabled.
 
+class SearchGrid :
+    def __init__(self, rows=5, columns=5, walls=[], diagonal=False ) : #walls -> [ list of index ]
+        self.rows = rows
+        self.columns = columns
+        self.N = rows * columns #total cells
+        self.walls = walls
+        self.diagonal = diagonal #falg to check if can move diagonally
+        
+    def edges(self) : 
+        """return edges of the graph in a list of tuples (u,v)"""
+        edges = []
 
+        #Just a convenient funtion
+        def _add_edge(u, v) :
+            #we add the edge if source and destinations are not walls
+            #and within the grid
+            if v % self.columns == 0 : #check if at the edge.
+                return
+            if u not in self.walls and v not in self.walls and u < self.N and v < self.N :
+                edges.append( (u,v) )
+        
+        #first the forward links to the right
+        for i in range(self.N) :
+            #if (i+1) % self.columns != 0 :#checking if it is an edge cell
+            _add_edge(i, i+1)            #connect to next cell
+            _add_edge(i,i+self.columns)  #connect to the cell below; it is i+width
+            if self.diagonal : #add diagonal edges as well
+                _add_edge(i,i+self.columns+1)  #connect to the cell below + 1
+                #_add_edge(i,i+self.columns-1)  #connect to the cell below + 1
+                
+        return edges
+    
+    #pretty print the grid and path if given. path -> [ list of nodes ]
+    def print(self, path=[]) :
+        for i in range(self.N) :
+            if i in self.walls :
+                print('# ', end='')
+            elif i in path :
+                print('^ ', end='')
+            else :
+                print('. ', end='')
+            if (i+1) % self.columns == 0 :
+                print("")
+#build a graph
 def graph(l):
     _graph = defaultdict(deque)
     for k, v in l:
@@ -39,26 +73,35 @@ def graph(l):
 
 
 def connected(start, end, _graph):
-    current_graph = _graph
-    print(current_graph)
     _connected = False
     _stack = [start]
     _visited = []
-    current_item = 0
     while len(_stack):
-        node_value = current_graph.get(_stack[current_item])
-        if _stack[current_item] not in _visited:
-            _visited.append(_stack[current_item])
-            if end in _visited:
-                _connected = True
-                return _connected
-        # print(f'this is the current stack{_stack}')
-        _stack.remove(_stack[current_item])
-        # print(f'this is the current visited list :{_visited}')
-        if node_value:
-            _stack += [i for i in node_value if i not in _visited]
+        _start = _stack.pop(0)
+        if _start not in _visited:
+            _visited.append(_start)
+            if end == _start:
+                return True
+        # print(_visited)
+        node_value = _graph.get(_start)
 
+        if node_value:
+            for i in node_value:
+                if not (i in _visited):
+                    _stack.append(i)
+        print(_stack)
+            # print(f'this is the node_values :  {node_value}')
+            # _stack += [i for i in node_value if not (i in _visited)]
+            # print(f'this is the _stack : {_stack}')
     return _connected
 
 
-print(connected(2, 6, graph(l)))
+if __name__ == "__main__":
+    _searchGrid = SearchGrid(20,20,walls=[2,5,8,9,20])
+    # _searchGrid.print()
+    # G = nx.Graph()
+    # G.add_edges_from(_searchGrid.edges())
+    # print(G)
+    # print(connected(2,8,graph(l)))
+    print(connected(0,20, graph(_searchGrid.edges())))
+    # print(f'The values you\'ve provided are not not connect because they are : {isConnect}')
